059dda4493b13841825216952cd0f4b51f8d524f
Daria "Kira-sa" Sokolova <dashasokol@gmail.com>
2026-06-23 14:43:13 +0500

    11

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from calendar_generator.config import CalendarConfig
 from calendar_generator.renderer.calendar import render_png_base64
 
 def handler(event, context):
-    if event.httpmethod == 'GET':
+    if event['httpmethod'] == 'GET':
         cfg = CalendarConfig.from_event(event or {})
         png_b64 = render_png_base64(cfg)
         return {
